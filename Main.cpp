c57411175ae738b3c959ff05a377131c94066be3
Carlos Siu <47042324+Siumauricio@users.noreply.github.com>
2019-08-30 14:24:53 -0600

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,4 @@
 #include <SFML/Graphics.hpp>
-#include <time.h>
 using namespace sf;
 
 //tamano de pantalla
@@ -39,6 +38,11 @@ void SnakeMov()
 	if (dir == 2) s[0].x += 1;
 	if (dir == 3) s[0].y -= 1;
 
+	//if (dir == 0 || dir == 1 || dir == 3 || dir == 4) {
+	//	s[0].x == 0;
+	//	s[0].y == 0;
+	//}
+
 	//tamano de snake cuando come comida
 	if ((s[0].x == f.x) && (s[0].y == f.y))
 	{
@@ -50,11 +54,17 @@ void SnakeMov()
 
 
 	//Validacion entre paredes
-	/*if (s[0].x > N) s[0].x = 0;  if (s[0].x < 0) s[0].x = N;
-	if (s[0].y > M) s[0].y = 0;  if (s[0].y < 0) s[0].y = M;*/
+	if (s[0].x > N) s[0].x = 0;  if (s[0].x < 0) s[0].x = N;
+	if (s[0].y > M) s[0].y = 0;  if (s[0].y < 0) s[0].y = M;
 
-	/*for (int i = 1; i < num; i++)
-		if (s[0].x == s[i].x && s[0].y == s[i].y)  num = i;*/
+
+	// validacion de piezas que se remueven al tocarse el mismo
+	for (int i = 1; i < num; i++) {
+		if (s[0].y == s[i].y) {
+			num = 0;
+			system("puase");
+		}
+	}
 }
 
 int main()
@@ -72,7 +82,7 @@ int main()
 	Sprite sprite2(t2);
 
 	Clock clock;
-	float timer = 0, delay = 0.1;
+	float tiempo = 0, delay = 0.1;
 
 	//posicion de comida
 	f.x = 20;
@@ -83,7 +93,7 @@ int main()
 	{
 		float time = clock.getElapsedTime().asSeconds();
 		clock.restart();
-		timer += time;
+		tiempo += time;
 
 
 		Event e;//Creacion de eventos
@@ -99,10 +109,10 @@ int main()
 		if (Keyboard::isKeyPressed(Keyboard::Down)) dir = 0;
 
 		//Andir movimiento
-		if (timer > delay) {
+		if (tiempo > delay) {
 
 			//movimiento snake
-			timer = 0;
+			tiempo = 0;
 			// algoritmo de movimiento
 			SnakeMov();
 		}
@@ -122,6 +132,6 @@ int main()
 
 		window.display();
 	}
-
+	     
 	return 0;
 }
